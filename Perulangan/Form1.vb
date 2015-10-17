24199f4f634611793ea5fa4c82c82058a4a28752
ZahraMJ <zahramj3@gmail.com>
2015-10-17 23:32:20 +0700

    Tugas Perulangan
    Tugas Perulangan Zahra Aulia M. Jannah

--- Perulangan/Form1.vb
+++ Perulangan/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
 
+    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintang.Click
+        Bintang.MdiParent = Me
+        Bintang.Show()
+    End Sub
+
+    Private Sub MatriksKalkulator_Click(sender As Object, e As EventArgs) Handles MatriksKalkulator.Click
+        Matriks.MdiParent = Me
+        Matriks.Show()
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub AplikasiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AplikasiToolStripMenuItem.Click
+
+    End Sub
 End Class
